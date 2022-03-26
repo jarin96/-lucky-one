92c4daa163ae832ae6b8ec3ea360211c5b36ddfb
jarin96 <2017-1-60-027@std.ewubd.edu>
2022-03-26 13:59:02 +0600

    Question and answer part complete

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -32,7 +32,6 @@ const Shop = () => {
                         key={product.id}
                         product={product}
                         handleAddToCart={handleAddToCart}
-
                     ></Product>)
                 }
             </div>
@@ -43,6 +42,9 @@ const Shop = () => {
             </div>
             <div className='details'>
                 <p>How React Works?</p>
+                <p>To work in react there are two packages one is React and another is React Dom.When a component is written , it is a pure javascript code.This javascript code is need to be convert in HTML code.So to convert javascript code into react code we need React package.React automatically convert a component(javascript code) into HTML code.This HTML code is showed to browser using React Dom.This is how react works.</p>
+                <p>Difference between props and state?</p>
+                <p>Props are read only, props value can not be changed.In props you can pass properties from parent component.Props are sent by unidirectional way means from up to down.Props can not be modified.On the other hand state can be changed.State changes can be asynchronous.State can be defined in the component itself.State can be modified using this.setState.</p>
             </div>
 
         </div>
